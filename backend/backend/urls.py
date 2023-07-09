8b3b8726432d2ed9cbd0ba60ed423c547415217a
eduardoperetto <erperetto@gmail.com>
2023-07-09 12:26:03 -0300

    feat: jwt login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,6 +4,10 @@ from django.views.static import serve
 from django.urls import include, path, re_path
 from rest_framework import routers
 from pathlib import Path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,4 +23,7 @@ urlpatterns = [
     path('', include(router.urls)),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': os.path.join(BASE_DIR, 'static')}), 
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
