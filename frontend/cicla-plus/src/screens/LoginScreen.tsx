5d74788dbdb788c985ea8b4733358cd294f62905
Sofia Braga <sofia.braga@sap.com>
2023-08-15 19:43:53 -0300

    creating login screen

--- frontend/cicla-plus/src/screens/LoginScreen.tsx
+++ frontend/cicla-plus/src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@material-tailwind/react";
+import { Button, Input } from "@material-tailwind/react";
 import { useDispatch } from "../store/configureStore";
 import { loginAction } from "../actions/login";
 
@@ -7,14 +7,25 @@ export default function LoginScreen() {
   const dispatch = useDispatch();
 
   return (
-    <>
-      <Button
+    <div className="flex w-screen h-screen justify-center items-center">
+      <div className="flex flex-col items-center gap-4">
+      <Input size="md" label="E-mail" />
+      <Input size="md" label="Senha" />
+        <Button
+          onClick={async () =>
+            await dispatch(loginAction({ username: "foo", password: "bar" }))
+          }
+        >
+          Entrar
+        </Button>
+        <Button
         onClick={async () =>
           await dispatch(loginAction({ username: "foo", password: "bar" }))
         }
-      >
-        Login
-      </Button>
-    </>
+        >
+          Registrar-se
+        </Button>
+      </div>
+    </div>
   );
 }
