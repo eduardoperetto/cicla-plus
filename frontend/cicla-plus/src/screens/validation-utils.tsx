550cd9c9dd0204f062e36df2b4a8cfa3c8de00f1
Lucca Kroeff <luccafranke1@gmail.com>
2023-08-23 21:39:31 -0300

    chore: add cedilha to regex

--- frontend/cicla-plus/src/screens/validation-utils.tsx
+++ frontend/cicla-plus/src/screens/validation-utils.tsx
@@ -7,19 +7,19 @@ export const validateField = (value: any, type: string, setErrorFunction: any, e
             regex = /^$|^[a-zA-Z0-9._-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
             break;
         case "sobrenome":
-            regex = /^$|^[a-zA-Z ]{2,}$/;
+            regex = /^$|^[a-zA-ZçÇ ]{2,}$/;
             break;
         case "nome":
-            regex = /^$|^[a-zA-Z ]{2,}$/;
+            regex = /^$|^[a-zA-ZçÇ ]{2,}$/;
             break;
         case "rua":
-            regex = /^$|^[a-zA-Z0-9 ]{3,}$/;
+            regex = /^$|^[a-zA-ZçÇ0-9 ]{3,}$/;
             break;
         case "cidade":
-            regex = /^$|^[a-zA-Z ]{3,}$/;
+            regex = /^$|^[a-zA-ZçÇ ]{3,}$/;
             break;
         case "bairro":
-            regex = /^$|^[a-zA-Z ]{3,}$/;
+            regex = /^$|^[a-zA-ZçÇ ]{3,}$/;
             break;
         case "cep":
             regex = /^$|^[0-9\-]{8}$/;
