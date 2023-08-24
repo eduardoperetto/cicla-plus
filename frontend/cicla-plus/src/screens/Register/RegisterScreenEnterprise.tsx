1181712c468472caec1bd5e617a910288fe60462
I585963 <lucca.kroeff@sap.com>
2023-08-23 23:41:21 -0300

    chore: change interface

--- frontend/cicla-plus/src/screens/Register/RegisterScreenEnterprise.tsx
+++ frontend/cicla-plus/src/screens/Register/RegisterScreenEnterprise.tsx
@@ -64,11 +64,11 @@ export default function RegisterScreen() {
         <div className="space-y-8">
           <div className="border-b border-gray-900/10 pb-8">
             <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="sm:col-span-3">
+              <div className="sm:col-span-2">
                 <Input size="md" label="Nome da Empresa" />
               </div>
 
-              <div className="sm:col-span-3">
+              <div className="sm:col-span-2">
                 <Input
                   size="md"
                   label="CNPJ"
@@ -86,6 +86,13 @@ export default function RegisterScreen() {
                 {CNPJError && (
                   <span className="text-red-500 text-sm">{CNPJError}</span>
                 )}
+              </div>
+
+              <div className="sm:col-span-2">
+                <Input
+                  size="md"
+                  label="Localização"
+                  ></Input>
               </div>
 
               <div className="sm:col-span-4">
@@ -108,117 +115,13 @@ export default function RegisterScreen() {
                 )}
               </div>
 
-              <div className="sm:col-span-3">
-                <Select label="Estado">
-                  <Option> </Option>
-                  <Option>AC</Option>
-                  <Option>AL</Option>
-                  <Option>AP</Option>
-                  <Option>AM</Option>
-                  <Option>BA</Option>
-                  <Option>CE</Option>
-                  <Option>DF</Option>
-                  <Option>ES</Option>
-                  <Option>GO</Option>
-                  <Option>MA</Option>
-                  <Option>MT</Option>
-                  <Option>MS</Option>
-                  <Option>MG</Option>
-                  <Option>PA</Option>
-                  <Option>PB</Option>
-                  <Option>PR</Option>
-                  <Option>PE</Option>
-                  <Option>PI</Option>
-                  <Option>RJ</Option>
-                  <Option>RN</Option>
-                  <Option>RS</Option>
-                  <Option>RO</Option>
-                  <Option>RR</Option>
-                  <Option>SC</Option>
-                  <Option>SP</Option>
-                  <Option>SE</Option>
-                  <Option>TO</Option>
-                </Select>
-              </div>
-              <div className="col-span-full">
-                <Input
-                  size="md"
-                  label="Rua"
-                  value={rua}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "rua",
-                      setRuaError,
-                      "Rua inválida",
-                      setRua
-                    )
-                  }
-                />
-                {ruaError && (
-                  <span className="text-red-500 text-sm">{ruaError}</span>
-                )}
-              </div>
-
-              <div className="sm:col-span-2 sm:col-start-1">
-                <Input
-                  size="md"
-                  label="Cidade"
-                  value={cidade}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "cidade",
-                      setCidadeError,
-                      "Cidade inválida",
-                      setCidade
-                    )
-                  }
-                />
-                {cidadeError && (
-                  <span className="text-red-500 text-sm">{cidadeError}</span>
-                )}
-              </div>
 
               <div className="sm:col-span-2">
                 <Input
                   size="md"
-                  label="Bairro"
-                  value={bairro}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "bairro",
-                      setBairroError,
-                      "Bairro inválido",
-                      setBairro
-                    )
-                  }
-                />
-                {bairroError && (
-                  <span className="text-red-500 text-sm">{bairroError}</span>
-                )}
-              </div>
-
-              <div className="sm:col-span-2">
-                <Input
-                  size="md"
-                  label="CEP"
-                  value={cep}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "cep",
-                      SetCepError,
-                      "CEP inválido",
-                      setCep
-                    )
-                  }
-                />
-                {cepError && (
-                  <span className="text-red-500 text-sm">{cepError}</span>
-                )}
-              </div>
+                  label="Telefone"
+                  ></Input>
+                  </div>
 
               <div className="sm:col-span-3">
                 <Input
--- frontend/cicla-plus/src/screens/Register/RegisterScreenPerson.tsx
+++ frontend/cicla-plus/src/screens/Register/RegisterScreenPerson.tsx
@@ -151,117 +151,18 @@ export default function RegisterScreen() {
               </div>
 
               <div className="sm:col-span-2">
-                <Select label="Estado">
-                  <Option> </Option>
-                  <Option>AC</Option>
-                  <Option>AL</Option>
-                  <Option>AP</Option>
-                  <Option>AM</Option>
-                  <Option>BA</Option>
-                  <Option>CE</Option>
-                  <Option>DF</Option>
-                  <Option>ES</Option>
-                  <Option>GO</Option>
-                  <Option>MA</Option>
-                  <Option>MT</Option>
-                  <Option>MS</Option>
-                  <Option>MG</Option>
-                  <Option>PA</Option>
-                  <Option>PB</Option>
-                  <Option>PR</Option>
-                  <Option>PE</Option>
-                  <Option>PI</Option>
-                  <Option>RJ</Option>
-                  <Option>RN</Option>
-                  <Option>RS</Option>
-                  <Option>RO</Option>
-                  <Option>RR</Option>
-                  <Option>SC</Option>
-                  <Option>SP</Option>
-                  <Option>SE</Option>
-                  <Option>TO</Option>
-                </Select>
+                <Input
+                  size="md"
+                  label="Localização"
+                  ></Input>
               </div>
 
               <div className="sm:col-span-2">
                 <Input
                   size="md"
-                  label="Cidade"
-                  value={cidade}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "cidade",
-                      setCidadeError,
-                      "Cidade inválida",
-                      setCidade
-                    )
-                  }
-                />
-                {cidadeError && (
-                  <span className="text-red-500 text-sm">{cidadeError}</span>
-                )}
-              </div>
-
-              <div className="col-span-full">
-                <Input
-                  size="md"
-                  label="Rua"
-                  value={rua}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "rua",
-                      setRuaError,
-                      "Rua inválida",
-                      setRua
-                    )
-                  }
-                />
-                {ruaError && (
-                  <span className="text-red-500 text-sm">{ruaError}</span>
-                )}
-              </div>
-
-              <div className="sm:col-span-2">
-                <Input
-                  size="md"
-                  label="Bairro"
-                  value={bairro}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "bairro",
-                      setBairroError,
-                      "Bairro inválido",
-                      setBairro
-                    )
-                  }
-                />
-                {bairroError && (
-                  <span className="text-red-500 text-sm">{bairroError}</span>
-                )}
-              </div>
-
-              <div className="sm:col-span-2">
-                <Input
-                  size="md"
-                  label="CEP"
-                  value={cep}
-                  onChange={(e) =>
-                    validateField(
-                      e.target.value,
-                      "cep",
-                      SetCepError,
-                      "CEP inválido",
-                      setCep
-                    )
-                  }
-                />
-                {cepError && (
-                  <span className="text-red-500 text-sm">{cepError}</span>
-                )}
-              </div>
+                  label="Telefone"
+                  ></Input>
+                  </div>
 
               <div className="sm:col-span-3">
                 <Input
